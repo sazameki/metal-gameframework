fa286847ec6929c102ddccd14267f87b5f76388c
Sazameki <info@sazameki.jp>
2018-06-09 14:07:31 +0900

    Time構造体にドキュメントコメントを追加しました。

--- MyMetalGame/Game Framework/Time.hpp
+++ MyMetalGame/Game Framework/Time.hpp
@@ -13,11 +13,22 @@
 /// ゲーム内の時間情報を表す構造体です。
 struct Time
 {
+    /// ゲーム開始から直前のフレームまでに経過したフレーム数
     static int      frameCount;
+
+    /// ゲーム開始から現在のフレームの実行開始までに経過した時間
     static float    time;
+
+    /// 直前のフレームの処理にかかった時間
     static float    deltaTime;
+
+    /// 時間の経過をスケールさせます。デフォルト値は1.0です。
     static float    timeScale;
+
+    /// timeScaleの値によってスケールしない、直前のフレームの処理にかかった時間です。
     static float    unscaledDeltaTime;
+
+    /// timeScaleの値によってスケールしない、ゲーム開始から現在のフレームの実行開始までに経過した時間
     static float    unscaledTime;
 
     static void     __Update();
